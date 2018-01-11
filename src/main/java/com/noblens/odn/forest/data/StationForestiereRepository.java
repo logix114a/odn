872554af128b90861ff62a59740dbd00553b9029
olivier <olivier@192.168.1.48>
2018-01-11 22:02:27 +0100

    New version

---
+++ src/main/java/com/noblens/odn/forest/data/StationForestiereRepository.java
@@ -0,0 +1,8 @@
+package com.noblens.odn.forest.data;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface StationForestiereRepository extends CrudRepository<StationForestiere, Long>{
+
+
+}
